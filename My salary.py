f186b8a349e76cf335ee0c9fbec8c067425e97cb
s7070018 <s7070018@kmitl.ac.th>
2014-11-18 14:07:54 +0700

    My salary

---
+++ My salary.py
@@ -0,0 +1,45 @@
+from Tkinter import *
+import tkMessageBox
+class App(object):
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("300x150+600+300")
+        self.root.wm_title("My salary")
+        Label (self.root, text= "Enter your username. : ").place(relx=0.05, rely=0.2)
+        Label (self.root, text= "Enter your password. : ").place(relx=0.05, rely=0.4)
+        
+
+        self.entryuser = StringVar()
+        Entry(self.root, textvariable=self.entryuser).place(relx=0.5, rely=0.2)
+
+
+        self.entrypass = StringVar()
+        Entry(self.root, show='*', textvariable=self.entrypass).place(relx=0.5, rely=0.4)
+
+        self.buttontext = StringVar()
+        self.buttontext.set("Login")
+        Button(self.root, textvariable=self.buttontext, command=self.open_new)\
+                          .place(relx=0.5, rely=0.8, anchor=CENTER)
+
+        self.root.mainloop()
+    def open_new(self):
+        self.root.destroy()
+        Windows()
+
+
+##    def clicked1(self):
+##        input = self.entrytext.get()
+##        result = int(input)*2
+##        self.label.configure(text=result)
+        
+##    def button_click(self):
+##        tkMessageBox.showwarning("Error", "Bad username and password")
+        
+class Windows(object):
+    def __init__(self):
+        self.win2 = Tk()
+        self.win2.geometry("600x300+500+250")
+
+        
+if __name__ == '__main__':
+    App()
