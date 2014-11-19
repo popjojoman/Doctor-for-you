2ff6f437e008cfc1adbe4e60e4ca0d0170eafd21
popjojoman <s7070067@kmitl.ac.th>
2014-11-19 19:24:24 +0700

    add week class

--- My salary.py
+++ My salary.py
@@ -45,12 +45,57 @@ class App(object):
         
 ##    def button_click(self):
 ##        tkMessageBox.showwarning("Error", "Bad username and password")
-        
+
 class Pattern_week(object):
     def __init__(self):
-        self.week = Tk()
-        self.week.geometry("600x300+500+250")
-        self.week.wm_title("My salary")
+        week = Tk()
+        week.geometry("600x300+500+250")
+        week.wm_title("My salary")
+        Label(week, text = 'earnings', )
+        listtime = [0,1,2,3,4,5,6,7,8,9]
+        
+        def show(self, index, mode):
+            num = week.globalgetvar(self)
+            if num == 2:
+                week.hourright = StringVar(week)
+                week.hourright.set(listtime[0])
+                OptionMenu(week,week.hourright, *listtime[0:4]).place(relx=0.19, rely=0.35)
+            else:
+                week.hourright = StringVar(week)
+                week.hourright.set(listtime[0])
+                OptionMenu(week,week.hourright, *listtime).place(relx=0.19, rely=0.35)
+                
+        
+        week.geometry("600x300+500+250")
+        week.wm_title("My salary")
+        Label(week, text = 'Earnings Per Hours :', ).place(relx=0.25, rely=0.1)
+
+        self.earn= StringVar(week)
+        Entry(week, textvariable=self.earn).place(relx=0.45, rely=0.1)
+        
+        
+        self.hourleft = StringVar(week)
+        self.hourleft.trace('w', show)
+        
+        self.hourleft.set(listtime[0])
+        OptionMenu(week,self.hourleft, *listtime[0:3]).place(relx=0.1, rely=0.35)
+
+        self.hourright = StringVar(week)
+        self.hourright.set(listtime[0])
+        OptionMenu(week,self.hourright, *listtime).place(relx=0.19, rely=0.35)
+        
+        
+        Label(week, text = ':', ).place(relx=0.29, rely=0.36)
+
+        self.minuteleft = StringVar(week)
+        self.minuteleft.set(listtime[0])
+        OptionMenu(week,self.minuteleft, *listtime[:6]).place(relx=0.32, rely=0.35)
+
+        self.minuteright = StringVar(week)
+        self.minuteright.set(listtime[0])
+        OptionMenu(week,self.minuteright, *listtime).place(relx=0.41, rely=0.35)
+        
+        week.mainloop()
 
 class Pattern_month(object):
     def __init__(self):
