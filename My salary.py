9997614ae4fe7f49c9b45e130e300b4ee75f7b60
popjojoman <s7070067@kmitl.ac.th>
2014-12-08 16:19:18 +0700

    add breaktime and workday

--- My salary.py
+++ My salary.py
@@ -16,7 +16,7 @@ class App(object):
         Label(self.root, text = 'Please Choose form').place(relx=0.5, rely=0.25, anchor = CENTER)       
 
         
-        Button(self.root, text = 'Week form', height = 2, width = 25, command = self.open_week)\
+        Button(self.root, text = 'Week form', height = 2, width = 25, command = self.open_before_week)\
                           .place(relx=0.5, rely=0.5,anchor = CENTER)
         Button(self.root, text = 'Month form', height = 2, width = 25, command = self.open_month)\
                           .place(relx=0.5, rely=0.75,anchor = CENTER)
@@ -27,11 +27,29 @@ class App(object):
        # Button(self.root, textvariable=self.buttontext, command=self.open_new)\
         #                  .place(relx=0.5, rely=0.8, anchor=CENTER)
 
+
         self.root.mainloop()
+    def open_before_week(self):
+        def open_week():
+            self.root.destroy()
+            ask_work.destroy()
+            Pattern_week()
+        ask_work = Tk()
+        ask_work.geometry("300x200+600+300")
+        ask_work.wm_title("My salary")
+        Label(ask_work, text='How many days do you work in 1 week ? ').place(relx=0.15, rely=0.2)
+        workday = StringVar(ask_work)
+        Entry(ask_work, textvariable=workday).place(relx=0.3, rely=0.5)
+        Button(ask_work, text='OK', command=open_week).place(relx=0.46, rely=0.7)
+
+
+    
         
-    def open_week(self):
-        self.root.destroy()
-        Pattern_week()
+
+        
+        
+        
+    
 
     def open_month(self):
         self.root.destroy()
@@ -54,6 +72,10 @@ class Pattern_week(object):
         Label(week, text = 'earnings', )
         listtime = [0,1,2,3,4,5,6,7,8,9]
         
+
+
+
+  
 ##--------------------------- SUB IN -----------------------------------------------
 
         def w_hrr1(self, index, mode):
@@ -96,11 +118,11 @@ class Pattern_week(object):
             if set2 == 9:
                 week.minuteleft2 = StringVar(week)
                 week.minuteleft2.set(listtime[0])
-                OptionMenu(week,week.minuteleft2, listtime[0]).place(relx=0.82, rely=0.35)
+                OptionMenu(week,week.minuteleft2, *listtime[:6]).place(relx=0.82, rely=0.35)
 
                 week.minuteright2 = StringVar(week)
                 week.minuteright2.set(listtime[0])
-                OptionMenu(week,week.minuteright2, listtime[0]).place(relx=0.91, rely=0.35)
+                OptionMenu(week,week.minuteright2, *listtime[0:]).place(relx=0.91, rely=0.35)
             else:
                 week.minuteleft2 = StringVar(week)
                 week.minuteleft2.set(listtime[0])
@@ -171,7 +193,73 @@ class Pattern_week(object):
         self.minuteright2 = StringVar(week)
         self.minuteright2.set(listtime[0])
         OptionMenu(week,self.minuteright2, *listtime).place(relx=0.91, rely=0.35)
+##----------------------------Break------------------------------------------------------
+        label = Label(week, text= 'Do you take a break ?')
+        label.place(relx=0.39, rely=0.5)
+    ##------------------destroytext and replace textbox ---------------------------------
+        def button_yes():
+            label.destroy()
+            butt_yes.destroy()
+            butt_no.destroy()
+                
+            Label(week, text = '-', ).place(relx=0.49, rely=0.556)
+            Label(week, text = 'Break time', ).place(relx=0.455, rely=0.46)
+            
+            self.breaks = StringVar(week)
+            Entry(week, textvariable=self.breaks).place(relx=0.26, rely=0.55)
 
+            self.start= StringVar(week)
+            Entry(week, textvariable=self.start).place(relx=0.54, rely=0.55)
+
+            
+            butt_submit.place(relx=0.34, rely=0.69)
+            butt_cancel.place(relx=0.54, rely=0.69)
+        def button_no():
+            label.destroy()
+            butt_yes.destroy()
+            butt_no.destroy()
+
+            
+            butt_submit.place(relx=0.36, rely=0.6)
+            butt_cancel.place(relx=0.56, rely=0.6)
+        def new_windows():
+            def back_2_week():
+                butt_submit.destroy()
+                butt_cancel.destroy()
+                new_win.destroy()
+            new_win = Tk()
+            new_win.geometry("250x200+500+250")
+            new_win.wm_title("My salary")
+            
+            Label(new_win, text = 'Please select the number of working days').place(relx=0.038, rely=0.2)
+
+
+            work_day = StringVar(new_win)
+            work_day.set(0)
+            listweek = [1,2,3,4,5,6,7]
+            OptionMenu(new_win, work_day, *listweek).place(relx=0.4, rely=0.4)
+            Button(new_win,text='OK', command = back_2_week).place(relx=0.4, rely=0.7)
+
+        
+               
+
+        def back():
+            week.destroy()
+            App()
+            
+            
+               
+        
+        butt_yes = Button(week, text='Yes', command =button_yes)
+        butt_yes.place(relx=0.6, rely=0.5)
+        butt_no = Button(week, text='No', command = button_no)
+        butt_no.place(relx=0.65, rely=0.5)
+        butt_submit = Button(week, text = 'Submit', command = new_windows,  height = 1, width = 10)
+        butt_cancel = Button(week, text = 'Cancel',command = back,  height = 1, width = 10)
+
+        
+         
+       
 
     
         
